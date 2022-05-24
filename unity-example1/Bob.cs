bc9cfa628471b2e343b3ce246214b604d247a20f
SubstanceShift <54613628+MuhammedKAldin@users.noreply.github.com>
2022-05-24 00:40:06 -0700

    pushed example1

---
+++ unity-example1/Bob.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Bob 
+{
+
+    public Vector3 velocity;
+    public float gravity = 20f;
+    public Vector3 gravityDirection = new Vector3(0, 1, 0);
+    Vector3 dampingDirection;
+    public float drag;
+
+    // Start is called before the first frame update
+    public void ApplyGravity()
+    {
+        velocity -= gravityDirection * gravity * Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    public void ApplyDamping()
+    {
+        dampingDirection = -velocity;
+        dampingDirection *= drag;
+        velocity += dampingDirection;
+    }
+}
