c29e4d714b564de4ab450d2b0fc7956f94b5e77e
SubstanceShift <54613628+MuhammedKAldin@users.noreply.github.com>
2022-05-24 01:50:59 -0700

    pushing example2

---
+++ unity-example2/Bob.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Bob 
+{
+
+    public Vector3 velocity;
+    public float gravity = 20f;
+    public Vector3 gravityDirection = new Vector3(0, 1, 0);
+
+    // Simulating air ressistance which dicreases swinging velocity, via hitting player in opposite direction
+    Vector3 dampingDirection;
+    public float drag; // Maybe a 0.001
+
+    // Start is called before the first frame update
+    public void ApplyGravity()
+    {
+        velocity -= gravityDirection * gravity * Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    public void ApplyDamping()
+    {
+        dampingDirection = -velocity;
+        dampingDirection *= drag;
+        velocity += dampingDirection;
+    }
+}
---
+++ unity-example2/Pendulum.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Pendulum 
+{
+
+    public Transform bob_tr;
+    public Tether tether;
+    public Arm arm;
+    public Bob bob;
+    Vector3 previousPosition;
+
+    public void Initalise()
+    {
+        bob_tr.transform.parent = tether.tether_tr;
+        arm.length = Vector3.Distance(bob_tr.transform.localPosition, tether.position);
+    }
+
+   public Vector3 MoveBob(Vector3 pos, float time)
+   {
+        bob.velocity += GetConstrainedVelocity(pos, previousPosition, time);
+        // Lesson 1
+        bob.ApplyGravity();
+        // Lesson 2 => Air ressistance 
+        bob.ApplyDamping();
+
+        pos += bob.velocity * time;
+
+        // Modify the shortening in the constraints, which causes a surdden-Jumping behaviour in direction when it's going opposite direction in high speed => it get's pulled quickly as the constraint is short
+        if (Vector3.Distance(pos, tether.position) < arm.length)
+        {
+            pos = Vector3.Normalize(pos - tether.position) * arm.length;
+            arm.length = (Vector3.Distance(pos, tether.position));
+            return pos;
+        }
+
+        previousPosition = pos;
+        return pos;
+   }
+
+    public Vector3 GetConstrainedVelocity(Vector3 currentPos, Vector3 previousPosition, float time)
+    {
+        float distanceToTether;
+        Vector3 constrainedPosition;
+        Vector3 predictedPosition;
+
+        distanceToTether = Vector3.Distance(currentPos, tether.position);
+        if (distanceToTether > arm.length)
+        {
+            constrainedPosition = Vector3.Normalize(currentPos - tether.position) * arm.length;
+            predictedPosition = (constrainedPosition - previousPosition) / time;
+            return predictedPosition;
+        }
+        return Vector3.zero;
+    }
+
+}
+ 
